9b48ca24556e07b888fdd334cb99823b7a665b08
Stephane BARON <baron.stephane@gmail.com>
2017-03-17 19:28:29 +0100

    Akeneo 1.7

--- akeneo/apache/scripts/entrypoint.sh
+++ akeneo/apache/scripts/entrypoint.sh
@@ -44,7 +44,7 @@ if [ "$DO_INIT_AKENEO" = "yes" ]; then
 	}
 
 	echo 'Download akeneo ...'
-	: ${AKENEO_URL:="http://download.akeneo.com/pim-community-standard-v1.6-latest.tar.gz"}
+	: ${AKENEO_URL:="http://download.akeneo.com/pim-community-standard-v1.7-latest.tar.gz"}
 	curl -L -s "$AKENEO_URL" | tar xzf - --directory /var/www/html --strip-components 1
 	
 	cd /var/www/html
